6bd72120467a6b215f2d0c41d1b221e56d0139ca
Raniere Silva <raniere@rgaiacs.com>
2020-03-17 09:36:10 -0300

    Add  read_word_from_file and  insert_word_to_compressed_trie

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
 /* Call to trie */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "trie.h"
 
 int main(int argc, char *argv[]) {
     FILE *file;
-    TrieTree *tree = calloc(1, sizeof(TrieTree));
+    CompressedTrieTree *tree = calloc(1, sizeof(CompressedTrieTree));
+    int letter;
     char prefix[32];
 
     if(argc <= 1) {
@@ -16,18 +18,27 @@ int main(int argc, char *argv[]) {
         printf("Processing %s ...\n", argv[1]);
 
         file = fopen(argv[1], "r");
-        read_file(file, tree);
+        do{
+            letter = read_word_from_file(file, prefix);
+            if(letter >= 97 /* a */ && letter <= 122 /* z */){
+                insert_word_to_compressed_trie(prefix, tree);
+            }
+        }while(letter != EOF);
         fclose(file);
 
         printf("%s was loaded successfully.\n", argv[1]);
 
-        printf("Type the prefix or 0 to quit: ");
-        scanf ("%32s", prefix);
-        find_suggestions(
-            prefix,
-            find_prefix(prefix, tree)
-        );
+        
+        do{
+            printf("Type the prefix or 0 to quit: ");
+            scanf ("%32s", prefix);
+            if(prefix[0] != "0"){
+                /* Do search */
+            }
+        }while(prefix[0] != "0");
     }
-    
+ 
+    free_compressed_trie(tree);
+
     return 0;
 }
--- src/trie.c
+++ src/trie.c
@@ -82,3 +82,78 @@ void find_suggestions(char *prefix, TrieTree *tree){
         }
     }
 }
+
+int read_word_from_file(FILE *file, char *prefix){
+    int i = 0;
+    int letter;
+
+    do{
+        letter = fgetc(file);
+        
+        if(letter >= 65 /* A */ && letter <= 90 /* Z */){
+           letter+=32; /* transform the letter to lowercase */
+        }
+
+        if(letter >= 97 /* a */ && letter <= 122 /* z */){
+            prefix[i] = letter;
+            i++;
+        }
+    }while(letter >= 97 /* a */ && letter <= 122 /* z */);
+
+    prefix[i] = '\0';
+    printf("%s\n", prefix);
+    return letter;
+}
+
+void insert_word_to_compressed_trie(char *prefix, CompressedTrieTree *tree){
+    int i;
+    CompressedTrieTree *subtree;
+
+    /* Does the node has any children? */
+    if(tree->prefix[0] == '\0'){  /* YES */
+        /* Need to follow next node */
+        i = (int) prefix[0] - 97;
+        if(tree->next[i] == NULL){
+            /* Create new node */
+            subtree = calloc(1, sizeof(CompressedTrieTree)); /* initializes the memory to zeros */
+            subtree->is_end_of_word = 1;
+            subtree->numer_of_appearance = 1;
+            strcpy(subtree->prefix, &prefix[1]);
+            tree->next[i] = subtree;
+        }
+        else{
+            insert_word_to_compressed_trie(&prefix[1], tree->next[i]);
+        }
+    }
+    else{  /* NO */
+        /* Create new node for current one */
+        subtree = calloc(1, sizeof(CompressedTrieTree)); /* initializes the memory to zeros */
+        i = (int) tree->prefix[0] - 97;
+        subtree->is_end_of_word = 1;
+        subtree->numer_of_appearance = tree->numer_of_appearance;
+        tree->next[i] = subtree;
+
+        /* Reset current node */
+        tree->is_end_of_word = 0;
+        tree->numer_of_appearance = 0;
+        tree->prefix[0] = '\0';
+
+        /* Create new node for new word */
+        subtree = calloc(1, sizeof(CompressedTrieTree)); /* initializes the memory to zeros */
+        i = (int) prefix[0] - 97;
+        subtree->is_end_of_word = 1;
+        subtree->numer_of_appearance = 1;
+        tree->next[i] = subtree;
+    }
+}
+
+void free_compressed_trie(CompressedTrieTree *tree){
+    int i;
+    for(i = 0; i < 26; i++){
+        if(tree->next[i] != NULL){
+            free_compressed_trie(tree->next[i]);
+            tree->next[i] = NULL;
+        }
+    }
+    free(tree);
+}
